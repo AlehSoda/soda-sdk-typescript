b7cf7feb09a3030621dbcd4d21450c2b8cceedb4
AlehNat <natsevsky@gmail.com>
2024-07-18 13:47:44 +0200

    update onboarding contract address

--- lib.commonjs/soda.d.ts
+++ lib.commonjs/soda.d.ts
@@ -1,4 +1,4 @@
 export { generateRSAKeyPair, encryptAES, decryptAES, decryptValue, decryptRSA, prepareMessage, signRawMessage } from "./crypto/index.js";
-export declare const ONBOARDING_CONTRACT_DEVNET_ADDRESS = "0xBaeC693DEE087dD1aD212640897EcD2f6510991f";
+export declare const ONBOARDING_CONTRACT_DEVNET_ADDRESS = "0xa7fdb3DeC5054E1ad1D678F9e9E88d03F94DA8f8";
 export declare const ONBOARDING_CONTRACT_ABI: string[];
 //# sourceMappingURL=soda.d.ts.map
--- lib.esm/soda.js
+++ lib.esm/soda.js
@@ -1,5 +1,5 @@
 export { generateRSAKeyPair, encryptAES, decryptAES, decryptValue, decryptRSA, prepareMessage, signRawMessage } from "./crypto/index.js";
-export const ONBOARDING_CONTRACT_DEVNET_ADDRESS = "0xBaeC693DEE087dD1aD212640897EcD2f6510991f";
+export const ONBOARDING_CONTRACT_DEVNET_ADDRESS = "0xa7fdb3DeC5054E1ad1D678F9e9E88d03F94DA8f8";
 export const ONBOARDING_CONTRACT_ABI = [
     "function getUserKey(bytes signedEK, bytes signature) public",
     "event UserKey(address indexed _owner, bytes _userKey)"
